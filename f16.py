7b9f03ce77fe0f539b9bd785b9e216f156d151b6
Jayshri15 <83698383+Jayshri15@users.noreply.github.com>
2021-05-27 15:52:44 +0530

    Add files via upload

---
+++ f16.py
@@ -0,0 +1,21 @@
+# def break_into_words(sentence):
+#     a=sentence.split()
+#     return a
+# sentence="NavGurukul is an alternative to higher education reducing the baeeiers of current formal education system"
+# print(break_into_words(sentence))
+# words="navgurukul is great"
+# counter=0
+# while counter<len(words):
+#     print(words[counter])
+#     counter=counter+1
+    
+
+
+mycode=("hello world")
+code"""
+def mutiply(x,y):
+    return x*y
+print("Multiple of 2 and 3 is:",mutiply(2,3))
+exec(mycode)
+exec(code)
+
