13b943e004462bf2283e9fa994d4298d90c66cca
Jayshri15 <83698383+Jayshri15@users.noreply.github.com>
2021-05-27 15:47:35 +0530

    Add files via upload

---
+++ f14.py
@@ -0,0 +1,15 @@
+# big_list= [[45, 21, 42, 63], [12, 42, 42, 53], [4, 90, 78, 13],[35,9,2,6]]
+
+# counter1 = 0
+# while counter1 < len(big_list):
+#     small_list_length = len(big_list[counter1])
+#     counter2 = 0
+#     while counter2 < small_list_length:
+#         print (big_list[counter1][counter2])
+#         counter2 = counter2 + 2
+#     counter1 = counter1 + 1
+#     print ('-----')
+    
+
+
+
