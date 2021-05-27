ec3949f81a8dfd641a9cb095c1f36e7ed320ee6d
Jayshri15 <83698383+Jayshri15@users.noreply.github.com>
2021-05-27 15:51:38 +0530

    Add files via upload

---
+++ f1.py
@@ -0,0 +1,15 @@
+def name():
+	i=0
+	j=0
+	list1=["hello","welcome"]
+	list2=["sonu","monu"]
+	while i<len(list1):
+		if list1[i]==list1[i]:
+			print(list1[1])
+			i=i+2
+		while j<len(list2):
+			if list2[j]==list2[j]:
+				print(list2[j])
+				j=j+1
+			
+name()
